a715f91d388f9f1524b41455075ee79b17de366b
papaiou <jacques.alzate@epitech.eu>
2017-03-03 22:34:15 +0100

    Add files via upload

---
+++ include/gnl.h
@@ -0,0 +1,20 @@
+/*
+** get_next_line.h for getnextline in /home/alzate_j/projects/CPE/CPE_2016_getnextline
+** 
+** Made by jacques alzate
+** Login   <alzate_j@epitech.net>
+** 
+** Started on  Tue Jan  3 10:09:00 2017 jacques alzate
+** Last update Fri Mar  3 22:31:46 2017 jacques alzate
+*/
+
+#ifndef READ_SIZE
+# define READ_SIZE (5)
+#endif /* !READ_SIZE */
+
+#ifndef GNL_H_
+# define GNL_H_
+
+char	*get_next_line(const int fd);
+
+#endif /* !GNL_H_ */
